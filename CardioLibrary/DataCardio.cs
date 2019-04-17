0d1f6768e1805b47f46437414ef020b056fc7d74
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-17 08:55:57 +0200

    Add project files.

---
+++ CardioLibrary/DataCardio.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardioLibrary
+{
+    public class DataCardio
+    {
+        public static double Btminimi(int età)
+        {
+            double battiti = 0;
+
+            if (età <= 0)
+            {
+                battiti = 0;
+            }
+            else
+            {
+                battiti = CalcoloBattiti(età);
+            }
+
+            return battiti;
+        }
+
+        private static double CalcoloBattiti(int età)
+        {
+            double battiti;
+            int frequenza = 220 - età;
+
+            battiti = frequenza * 0.7;
+            return battiti;
+        }
+
+        public static double Btmassimi(int età)
+        {
+            double battiti = 0;
+
+            if (età <= 0)
+            {
+                battiti = 0;
+            }
+            else
+            {
+                battiti = CalcoloBattitiDonna(età);
+            }
+
+            return battiti;
+        }
+
+        private static double CalcoloBattitiDonna(int età)
+        {
+            double battiti;
+            int frequenza = 220 - età;
+
+            battiti = frequenza * 0.9;
+            return battiti;
+        }
+
+        public static string FrequenzaCardiacaRiposo(double battiti)
+        {
+            string risultato = "";
+
+            if(battiti <= 0)
+            {
+                risultato = "Errore";
+            }
+            else
+            {
+                if (battiti < 60)
+                {
+                    risultato = "Bradicardia";
+                }
+
+                if (battiti >= 60 && battiti <= 100)
+                {
+                    risultato = "Normale";
+                }
+
+                if (battiti > 100)
+                {
+                    risultato = "Tachicardia";
+                }
+            }
+
+            return risultato;
+        }
+
+        public static double CalorieBruciate(string sesso, double frequenza_cardiaca, double peso, int anni, double tempo)
+        {
+            double calorie_bruciate = 0;
+
+            if (sesso == "M")
+            {
+                calorie_bruciate = ((anni * 0.2017) + (peso * 0.199) + (frequenza_cardiaca * 0.6309) - 55.0969) * tempo / 4.184;
+                //calorie_bruciate=Math.Round(calorie_bruciate, 2);
+            }
+
+            if (sesso == "F")
+            {
+                calorie_bruciate = ((anni * 0.074) - (peso * 0.126) + (frequenza_cardiaca * 0.4472) - 20.4022) * tempo / 4.184;
+                //Math.Round(calorie_bruciate, 2);
+            }
+
+            return Math.Round(calorie_bruciate, 2); 
+        }
+
+        public static double SpesaEnergetica(string attività, double km_percorsi, double peso_corporeo)
+        {
+            double risultato = 0;
+
+            if (attività == "Corsa")
+            {
+                risultato = (0.9 * km_percorsi) * peso_corporeo;
+            }
+
+            if (attività == "Camminata")
+            {
+                risultato = (0.50 * km_percorsi) * peso_corporeo;
+            }
+
+            return risultato;
+        }
+    }
+}
---
+++ DataCardio.Test/DataCardio_Test.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataCardio.Test
+{
+    [TestClass]
+    public class DataCardio_Test
+    {
+        //script 1
+        [DataTestMethod]
+        [DataRow(15, 143.5, 184.5)]  //età, battitti minimi, battiti massimi 
+        [DataRow(20, 140, 180)]
+        [DataRow(25, 136.5, 175.5)]
+        [DataRow(30, 133, 171)]
+
+        public void TestBattitiMinimiMassimi(int età, double battiti_min_aspettati, double battiti_max_aspettati)
+        {
+            double battiti_min = CardioLibrary.DataCardio.Btminimi(età);
+            Assert.AreEqual(battiti_min, battiti_min_aspettati);
+
+            double battiti_max = CardioLibrary.DataCardio.Btmassimi(età);
+            Assert.AreEqual(battiti_max, battiti_max_aspettati);
+        }
+
+        //script 2
+        [DataTestMethod]
+        [DataRow(40, "Bradicardia")]  //battiti, frequenza cardiaca a riposo
+        [DataRow(50, "Bradicardia")]
+        [DataRow(60, "Normale")]
+        [DataRow(100, "Normale")]
+        [DataRow(120, "Tachicardia")]
+        [DataRow(140, "Tachicardia")]
+        [DataRow(-10, "Errore")]
+        [DataRow(0, "Errore")]
+
+        public void TestValoriFrequenzaCardiacaRiposo(double battiti, string frequenza_cardiaca_riposo_aspettati)
+        {
+            string frequenza_cardiaca_riposo = CardioLibrary.DataCardio.FrequenzaCardiacaRiposo(battiti);
+
+            Assert.AreEqual(frequenza_cardiaca_riposo, frequenza_cardiaca_riposo_aspettati);
+        }
+
+        //script 3
+        [DataTestMethod]
+        [DataRow("M", 80, 75, 40, 60, 263.40)]  //sesso, frequenza cardiaca media, peso, anni, durata sessione allenamento
+        [DataRow("F", 60, 65, 30, 60, 6.59)]
+
+        public void TestCalorieBruciate(string sesso, double frequenza_cardiaca, double peso, int anni, double tempo, double calorie_bruciate_aspettate)
+        {
+            double calorie_bruciate = CardioLibrary.DataCardio.CalorieBruciate(sesso, frequenza_cardiaca, peso, anni, tempo);
+
+            Assert.AreEqual(calorie_bruciate_aspettate, calorie_bruciate);
+        }
+
+        //script 4
+        [DataTestMethod]
+        [DataRow("Corsa", 10, 80, 720)]  //tipo di attività, km percorsi, peso corporeo, spesa energetica
+        [DataRow("Camminata", 20, 70, 700)]
+
+        public void TestSpesaEnergetica(string attività, double km_percorsi, double peso_corporeo, double spesa_energetica_aspettata)
+        {
+            double spesa_energetica = CardioLibrary.DataCardio.SpesaEnergetica(attività, km_percorsi, peso_corporeo);
+
+            Assert.AreEqual(spesa_energetica_aspettata, spesa_energetica);
+        }
+    }
+}
